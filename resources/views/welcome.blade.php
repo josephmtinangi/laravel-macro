c80dbbbf7a689d1c06bb75c209603436c2ebcdf8
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-07-04 12:17:20 +0300

    Show users with latest post and all posts

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,48 @@
-<!doctype html>
-<html lang="{{ config('app.locale') }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Laravel</title>
+@section('content')
+    <div class="container-fluid">
+        @foreach($users->chunk(3) as $userSet)
+            <div class="row">
+                @foreach($userSet as $user)
+                    <div class="col-sm-4">
+                    
+                        <div class="panel panel-default">
+                        
+                            <div class="panel-heading">
+                            
+                                <img src="https://gravatar.com/avatar/{{ md5(strtolower(trim($user->email))) }}" class="img-responsive img-circle" alt="Avatar of {{ $user->name }}">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+                                <h2>{{ $user->name }}</h2>
+                                    <p>
+                                    <small>{{ $user->email }}</small>
+                                    </p>
+                                <div class="well">
+                                    <h4>Latest Post</h4>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+                                    {{ $user->lastPost->title }}
+                                    </div>
+                                <div>
 
-            .full-height {
-                height: 100vh;
-            }
+                            </div>                        
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+                            <div class="panel-body" style="max-height: 200px; overflow: scroll;">
+                            
+                                <h4>All Posts</h4>
 
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                                    @foreach($user->posts as $post)
+                                    <h4>{{ $post->title }}</h4>
+                                    <small>{{ $post->created_at->diffForHumans() }}</small>
+                                    @endforeach
+                                </div>                             
+                            
+                            </div>
+                        
+                        </div>
+                    
+                    </div>
+                @endforeach
             </div>
-        </div>
-    </body>
-</html>
+        @endforeach        
+    </div>
+@endsection
