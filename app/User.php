3725098fe55527080e790d118d619e76c7132b8f
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-07-04 11:52:59 +0300

    Create macro provider

--- app/User.php
+++ app/User.php
@@ -26,4 +26,14 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function posts()
+    {
+        return $this->hasMany('App\Models\Post');
+    }
+
+    public function lastPost()
+    {
+        return $this->posts()->latest()->toHasOne();
+    }
 }
--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -22,3 +22,13 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Models\Post::class, function (Faker\Generator $faker) {
+    return [
+        'title' => $faker->sentence,
+        'body' => $faker->paragraph,
+        'user_id' => function () {
+            return factory('App\User')->create()->id;
+        },
+    ];
+});
